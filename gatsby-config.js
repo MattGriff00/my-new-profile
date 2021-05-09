e5c8e4eae8bf45a511cf45b97e83d66ad9d0aa2b
MattGriff00 <72370376+MattGriff00@users.noreply.github.com>
2021-05-09 18:25:25 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // Of course you can also add new values here to query them like usual
+    siteTitle: `Develop-Griffin`,
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
     siteTitleAlt: `Develop-Griffin`,
   },
